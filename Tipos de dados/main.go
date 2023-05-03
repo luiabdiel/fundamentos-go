a6c52aa28c25a053ff28d122445eeff2b14ac301
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-03 12:54:27 -0300

    tipos e tamanhos de dados

--- Tipos de dados/main.go
+++ Tipos de dados/main.go
@@ -23,3 +23,21 @@ func main() {
 	fmt.Println(height)   // 1.70
 	fmt.Println(isFemale) // false
 }
+
+/*
+	int8 8-bit signed integer
+	int16 16-bit signed integer
+	int32 32-bit signed integer
+	int64 64-bit signed integer
+
+	uint8 8-bit unsigned integer
+	uint16 16-bit unsigned integer
+	uint32 32-bit unsigned integer
+	uint64 64-bit unsigned integer
+
+	int Both in and unit contain same size, either 32 or 64 bit
+	uint Both in and unit contain same size, either 32 or 64 bit
+
+	float32 32-bit IEEE 754 floating-point number
+	float64 64-bit IEEE 754 floating-point number
+*/
