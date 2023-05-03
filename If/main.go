b3b45a541d426beae906579d84bf1828434da79f
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-03 15:25:28 -0300

    if

---
+++ If/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	salario := 1320.00
+	var bonus float64
+
+	bonus = 0.10
+
+	if salario <= 1320 {
+		salario += (salario * bonus)
+		fmt.Println("Salário com bônus: ", salario)
+		return
+	}
+
+	fmt.Println("Salário: ", salario)
+}
