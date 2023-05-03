c0f538a55acd3fcdbf021efa9ab56e80a916387a
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-02 23:36:06 -0300

    tipos de dados

---
+++ Tipos de dados/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+	// var number int
+	// var height float32
+	// var isFemale bool
+
+	// fmt.Println(name) ""
+	// fmt.Println(number) 0
+	// fmt.Println(height) 0
+	// fmt.Println(isFemale) false
+
+	name := "Lei"
+	number := 10
+	height := 1.70
+	isFemale := false
+
+	fmt.Println(name)     // "teste"
+	fmt.Println(number)   // 10
+	fmt.Println(height)   // 1.70
+	fmt.Println(isFemale) // false
+}
