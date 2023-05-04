3bd20c4c361a34cdc5a072198189092b3d6b7f59
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-04 13:24:24 -0300

    map

--- Map/main.go
+++ Map/main.go
@@ -3,6 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string]int{"sp": 12000000, "ba": 15000000}
+	// m := map[string]int{"sp": 12000000, "ba": 15000000}
+	// fmt.Println(m)
+
+	m := make(map[string]int)
+
+	m["sp"] = 12000000
+	m["ba"] = 15000000
+	m["pr"] = 11000000
+
+	valor, foiEncontrado := m["rj"]
+	if foiEncontrado {
+		fmt.Println(valor)
+	} else {
+		fmt.Println("Essa chave não existe")
+	}
+
 	fmt.Println(m)
 }
