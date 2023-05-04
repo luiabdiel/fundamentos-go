403bcc077168d82f547d824a4dde4517b7308ae2
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-04 13:33:01 -0300

    range em map

---
+++ Range/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+
+	m["sp"] = 12000000
+	m["ba"] = 15000000
+	m["pr"] = 11000000
+
+	for chave, valor := range m {
+		fmt.Println(chave, "tem ", valor, "habitantes")
+	}
+}
