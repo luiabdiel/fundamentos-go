ff19da542a7ddd569c65c8a48eb0253eba93137d
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-11 15:14:19 -0300

    Sync.Mutex

---
+++ Sync.Mutex/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var m sync.Mutex
+
+	i := 0
+
+	for x := 0; x < 1000; x++ {
+		go func() {
+			m.Lock()
+			i++
+			m.Unlock()
+		}()
+	}
+
+	time.Sleep(time.Second * 5)
+	fmt.Println(i)
+}
