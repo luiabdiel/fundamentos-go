5926051c89fff67df02424f97d711b246cf13636
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-05-11 16:34:37 -0300

    channel

---
+++ Channel/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	channel := make(chan int)
+
+	go setList(channel)
+
+	for v := range channel {
+		fmt.Println(v)
+	}
+}
+
+func setList(channel chan int) {
+	for i := 0; i < 100; i++ {
+		channel <- i
+	}
+
+	close(channel)
+}
